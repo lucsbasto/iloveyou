94bd25f4ecfce3e39249cfa72b5db4e64c413358
Lucas Bastos <lucasbastos@gmail.com>
2020-03-24 13:13:00 -0300

    removendo os 100px de onde o usuário tocar

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-var event = "";
-console.log(screen.width);
-if (screen.width >= 1400) {
-  event = "mousemove";
+if (screen.width > 1000) {
+  document.addEventListener("mousemove", function(e) {
+    var body = document.querySelector("body");
+    var heart = document.createElement("span");
+    heart.setAttribute("class", "heart");
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var size = Math.random() * 100;
+    heart.style.left = x + "px";
+    heart.style.top = y + "px";
+    heart.style.width = 20 + size + "px";
+    heart.style.height = 20 + size + "px";
+    body.appendChild(heart);
+    setTimeout(function() {
+      heart.remove();
+    }, 1000);
+  });
 } else {
-  event = "touchmove";
+  document.addEventListener("touchmove", function(e) {
+    var body = document.querySelector("body");
+    var heart = document.createElement("span");
+    heart.setAttribute("class", "heart");
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var size = Math.random() * 10;
+    heart.style.left = x + "px";
+    heart.style.top = y + "px";
+    heart.style.width = size + "px";
+    heart.style.height = size + "px";
+    body.appendChild(heart);
+    setTimeout(function() {
+      heart.remove();
+    }, 1000);
+  });
 }
-
-document.addEventListener(event, function(e) {
-  var body = document.querySelector("body");
-  var heart = document.createElement("span");
-  heart.setAttribute("class", "heart");
-  var x = e.offsetX;
-  var y = e.offsetY;
-  var size = Math.random() * 100;
-  heart.style.left = x + "px";
-  heart.style.top = y + "px";
-  heart.style.width = 20 + size + "px";
-  heart.style.height = 20 + size + "px";
-  body.appendChild(heart);
-  setTimeout(function() {
-    heart.remove();
-  }, 1000);
-});
